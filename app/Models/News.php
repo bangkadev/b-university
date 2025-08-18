845080daf30099dae29d03cae90d3e2205f33ee9
Dicky April Zahdi <bangkadev@gmail.com>
2025-08-18 18:13:14 +0700

    Mengatur Fillable Guarded serta Relationship

--- app/Models/News.php
+++ app/Models/News.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title', 'content', 'slug', 'users_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'users_id');
+    }
 }
